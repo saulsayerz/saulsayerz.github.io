ea7e017b028b94c105a7b515521331bd95c4b112
Saul Sayers <saulsayers@gmail.com>
2022-12-06 18:27:26 +0700

    Updates

---
+++ precache-manifest.78803a92f3303522529af95f732f1e9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af46716bca1a19a9d039",
+    "url": "/static/css/main.6c1b9ef7.chunk.css"
+  },
+  {
+    "revision": "af46716bca1a19a9d039",
+    "url": "/static/js/main.af46716b.chunk.js"
+  },
+  {
+    "revision": "83d7a192298f03239cf7",
+    "url": "/static/js/1.83d7a192.chunk.js"
+  },
+  {
+    "revision": "fc6cb0553da5f721089d",
+    "url": "/static/js/2.fc6cb055.chunk.js"
+  },
+  {
+    "revision": "d36646d2f044db88dee9",
+    "url": "/static/js/runtime~main.d36646d2.js"
+  },
+  {
+    "revision": "e073bc479e74479b3ff8354ae50db25a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a9527a860349fbb124cf9899007d7c1.js"
+  "/precache-manifest.78803a92f3303522529af95f732f1e9c.js"
 );
 
 workbox.clientsClaim();
